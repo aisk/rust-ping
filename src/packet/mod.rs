fbcb8c6e62a796d94963fee1404be0fd40148979
Euan <euanmeston@gmail.com>
2024-03-21 10:33:10 +0000

    removed unused import

--- src/packet/mod.rs
+++ src/packet/mod.rs
@@ -3,4 +3,4 @@ mod ipv4;
 
 pub use self::icmp::{EchoReply, EchoRequest, IcmpV4, IcmpV6, HEADER_SIZE as ICMP_HEADER_SIZE};
 
-pub use self::ipv4::{IpV4Packet, IpV4Protocol};
+pub use self::ipv4::{IpV4Packet};
