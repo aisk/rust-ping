d6c155aa2d172c0662f1f6c432cc733ff533b194
AN Long <aisk@users.noreply.github.com>
2025-07-01 22:59:34 +0900

    🐛 Fix mutability of builder api and export socket type

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ mod packet;
 mod ping;
 
 pub use crate::errors::Error;
-pub use crate::ping::new;
-pub use crate::ping::{dgramsock, ping, rawsock};
+pub use crate::ping::{
+    dgramsock, new, ping, rawsock, Ping, SocketType, SocketType::DGRAM, SocketType::RAW,
+};
